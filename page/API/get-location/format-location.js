761dd8a00c83d2fb05529b7f869209ca890b86d4
zengwei <zengwei@heshetrends.com>
2016-11-04 19:14:39 +0800

    第一次修改

--- page/API/get-location/format-location.js
+++ page/API/get-location/format-location.js
@@ -1,6 +1,6 @@
 function formatLocation(longitude, latitude) {
-  longitude = longitude.toFixed(2)
-  latitude = latitude.toFixed(2)
+  longitude = longitude.toFixed(2);
+  latitude = latitude.toFixed(2);
 
   return {
     longitude: longitude.toString().split('.'),
